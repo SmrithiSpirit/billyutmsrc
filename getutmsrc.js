89bd988f8655bf5b03e140f3456820be8d657a31
SmrithiSpirit <45619581+SmrithiSpirit@users.noreply.github.com>
2021-01-18 13:40:18 +0530

    Update getutmsrc.js

--- getutmsrc.js
+++ getutmsrc.js
@@ -2,8 +2,7 @@ window.addEventListener('message', function(eventData) {
  try {
   if (JSON.parse(eventData.data)) {
   let event = JSON.parse(eventData.data);
-  console.log(event, "eventData1");
-  console.log(event.event_code === "custom-event" && event.data && event.data.code === "google-login", "condition");
+  
   if (event.event_code === "custom-event" && event.data && event.data.code === "google-login") {
     // let win = window.open("", "Payment", "toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,width=780,height=200,top=" + (screen.height - 400) + ",left=" + (screen.width - 840));
     // win.document.body.innerHTML = event.data.data;
@@ -12,6 +11,10 @@ window.addEventListener('message', function(eventData) {
       newWindow.document.close();
       return;
   }
+   if(event.event_code === "custom-event" && event.data && event.data.code === "utm-src"){
+    var x = localStorage.getItem("utm_source")
+    console.log(x)
+   }
 }
   return;
  } catch (error) {
